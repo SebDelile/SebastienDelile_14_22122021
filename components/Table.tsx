fb2dd450327cac87c61831c0eecb677891aa12e2
Delile Sebastien <sebastiendelile@gmail.com>
2021-12-30 19:05:30 +0100

    feat: use context in Table component

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,7 +1,11 @@
 import Datatable from 'react-datatable-generator';
 import { dataSample } from '../data/dataSample';
 import { headingsSample } from '../data/headingsSample';
+import { useGlobalContext } from '../utils/globalContext';
 
-export const Table = () => (
-  <Datatable headings={headingsSample} data={dataSample} />
-);
+export const Table = () => {
+  const { employeeList } = useGlobalContext();
+  return employeeList.length ? (
+    <Datatable headings={headingsSample} data={employeeList} />
+  ) : null;
+};
