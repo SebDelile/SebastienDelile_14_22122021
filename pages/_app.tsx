64eb6daad0904cb3cdea5c3dceab325d2a042e4c
Delile Sebastien <sebastiendelile@gmail.com>
2022-01-05 18:21:50 +0100

    feat: include globals tailwind css file into _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { GlobalContext } from '../utils/GlobalContext';
+import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
